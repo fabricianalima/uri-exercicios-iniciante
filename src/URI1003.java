731be04c619aef92dd05d98348f802b1e9d7d691
fabricianalima <fabricianacorreialima@gmail.com>
2021-04-18 13:24:44 -0300

    resolução do exercicio 1003

---
+++ src/URI1003.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class URI1003 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int a = teclado.nextInt();
+        int b = teclado.nextInt();
+        int resultado = a + b;
+        System.out.println("SOMA = " + resultado);
+    }
+}
