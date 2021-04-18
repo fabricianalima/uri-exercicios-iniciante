a22f0382ccfad1d989b157abb900026c47bbbfec
fabricianalima <fabricianacorreialima@gmail.com>
2021-04-18 13:11:57 -0300

    commit inicial

---
+++ src/URI1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class URI1001 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int a = ler.nextInt();
+        int b = ler.nextInt();
+        int x = a + b;
+        System.out.println("x = " + x);
+    }
+}
