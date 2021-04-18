7a7f370219f172fb87a7b12bca733b325605a03f
fabricianalima <fabricianacorreialima@gmail.com>
2021-04-18 14:38:45 -0300

    resolução do exercicio 1004

---
+++ src/URI1004.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class URI1004 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int a = teclado.nextInt();
+        int b = teclado.nextInt();
+        int resultadoProd = a * b;
+        System.out.println("PROD = " + resultadoProd);
+    }
+}
